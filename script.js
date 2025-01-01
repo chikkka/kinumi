6bc162ae9376f8968e8a4ae77ea5447c670de60b
chikkka <141250393+chikkka@users.noreply.github.com>
2025-01-02 08:23:26 +0900

    add script.js

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ let btn = document.querySelector(".toggle-btn")
 btn.onclick = () => {
   nav.classList.toggle("open");
 };
+
+// メニューバーの固定
+const menuBar = document.querySelector("#effectItem");
+const stickyPoint = menuBar.offsetTop;
+window.addEventListener("scroll",()=>{
+  if(window.scrollY >= stickyPoint){
+  menuBar.classList.add("fixed");
+}else{
+  menuBar.classList.remove("fixed");
+}
+});
+
+
+
+
